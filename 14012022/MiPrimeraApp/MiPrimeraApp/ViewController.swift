3e75a113d9572e915c1e7b84f15ec20693faf238
Carlos <ccarrera@nervia.local>
2022-02-01 17:30:20 +0100

    [INIT] - Created new App
    [INIT] - Created new App
    + Created app Mi primera App iOS

---
+++ 14012022/MiPrimeraApp/MiPrimeraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiPrimeraApp
+//
+//  Created by Carlos Carrera on 1/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
