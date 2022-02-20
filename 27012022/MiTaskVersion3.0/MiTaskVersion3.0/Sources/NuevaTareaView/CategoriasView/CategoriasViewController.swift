290aefc18396ff79b8e36bf8a5fa072f3f867801
Carlos <ccarrera@nervia.local>
2022-02-20 13:37:37 +0100

    [WIP] - MiTaskVersion recursos y clases

---
+++ 27012022/MiTaskVersion3.0/MiTaskVersion3.0/Sources/NuevaTareaView/CategoriasView/CategoriasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoriasViewController.swift
+//  MiTaskVersion3.0
+//
+//  Created by Carlos Carrera on 20/2/22.
+//
+
+import UIKit
+
+class CategoriasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
